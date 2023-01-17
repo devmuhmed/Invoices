e4b43874a5b0bea1dfe5c35294cc57941a276e74
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2023-01-18 00:38:42 +0200

    using -mcr for creating invoices and sections

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/{page}', 'AdminController@index');
+Route::get('pages/{page}', 'AdminController@index');
+Route::resource('invoices', 'InvoiceController');
+Route::resource('sections', 'SectionController');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
